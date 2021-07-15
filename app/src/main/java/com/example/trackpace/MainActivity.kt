76bef94fb0001be3e15050dad6c80b357523a969
atif9 <mohammadatif92080@gmail.com>
2021-07-16 00:51:38 +0530

    splashscreen launcher activity now

---
+++ app/src/main/java/com/example/trackpace/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.trackpace
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    /*
+    Start Date 10/07/2021
+     */
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
