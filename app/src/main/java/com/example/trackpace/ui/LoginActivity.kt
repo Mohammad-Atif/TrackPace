06fd63bd122d856e450f364c72ed73a60fd02ef9
atif9 <mohammadatif92080@gmail.com>
2021-09-27 00:20:03 +0530

    login and login check(splash screen) working

--- app/src/main/java/com/example/trackpace/ui/LoginActivity.kt
+++ app/src/main/java/com/example/trackpace/ui/LoginActivity.kt
@@ -1,25 +1,51 @@
 package com.example.trackpace.ui
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
+import com.example.trackpace.R
 import com.example.trackpace.databinding.ActivityLoginBinding
+import com.example.trackpace.util.Constants
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
 
     private var userName:String?=null
-    private var userAge: Int? = null
-    private var userGender: String? = null
+    private var userAge: Int = 18
+    private var userGender: String = "Male"
     private var userWeight:Int? = null
     private var userHeight:Int? = null
+    private lateinit var ageList:Array<Int>
+    private var hFt:Int = 0
+    private var hIn:Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        ageList = Array(80) { i -> i + 18 }
+
+
+
+        val spinner: Spinner = findViewById(R.id.sp_Age)
+        ArrayAdapter(
+            this,
+            android.R.layout.simple_spinner_item,
+            ageList
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinner.adapter = adapter
+        }
 
 //        Handler().postDelayed(
 //            {
@@ -29,9 +55,105 @@ class LoginActivity : AppCompatActivity() {
 //            },1600
 //        )
 
+        binding.spAge.onItemSelectedListener= object: AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(adapterView: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                userAge=adapterView?.getItemAtPosition(position).toString().toInt()
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+
+            }
+
+        }
+
+        binding.spGender.onItemSelectedListener = object: AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(adapterView: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                userGender=adapterView?.getItemAtPosition(position).toString()
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+
+            }
+
+        }
+
+        binding.spHeightFt.onItemSelectedListener = object: AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(adapterView: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                hFt=adapterView?.getItemAtPosition(position).toString().toInt()
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+
+            }
+
+        }
+
+        binding.spHeightIn.onItemSelectedListener = object: AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(adapterView: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                hIn=adapterView?.getItemAtPosition(position).toString().toInt()
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+
+            }
+
+        }
+
+        binding.btnStart.setOnClickListener {
+            signIn()
+        }
+
+
+
+
     }
 
+    private fun kgToPounds(weight: Int): Double {
+        return weight / 0.45359237
+    }
 
+    private fun toInches(feet:Int,inches:Int): Int {
+        return feet*12+inches
+    }
+
+    private fun signIn()
+    {
+        if(binding.editTextName.text.isNullOrBlank())
+        {
+            Toast.makeText(this,"Enter Name",Toast.LENGTH_SHORT).show()
+            binding.editTextName.requestFocus()
+            return
+        }
+        if(binding.textInputWeightEdit.text.isNullOrBlank())
+        {
+            Toast.makeText(this,"Enter Weight",Toast.LENGTH_SHORT).show()
+            binding.textInputWeightEdit.requestFocus()
+            return
+        }
+
+        if(hFt==0)
+        {
+            binding.spHeightFt.requestFocus()
+            return
+        }
+        userHeight=toInches(hFt,hIn)
+        userWeight=kgToPounds(binding.textInputWeightEdit.text.toString().toInt()).toInt()
+        val sharedPreference=getSharedPreferences("forcounts", Context.MODE_PRIVATE)
+        with(sharedPreference.edit())
+        {
+            putString(Constants.USER_NAME,binding.editTextName.text.toString())
+            putString(Constants.USER_GENDER, userGender)
+            putInt(Constants.USER_HEIGHT,userHeight!!)
+            putInt(Constants.USER_WEIGHT,userWeight!!)
+            putInt(Constants.USER_AGE, userAge)
+            apply()
+            commit()
+        }
+
+        val intent= Intent(this, MainActivity::class.java)
+        startActivity(intent)
+
+    }
 
 
 }
--- app/src/main/java/com/example/trackpace/ui/SplashScreen.kt
+++ app/src/main/java/com/example/trackpace/ui/SplashScreen.kt
@@ -1,10 +1,12 @@
 package com.example.trackpace.ui
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import com.example.trackpace.databinding.ActivitySplashScreenBinding
+import com.example.trackpace.util.Constants
 
 class SplashScreen : AppCompatActivity() {
 
@@ -13,11 +15,25 @@ class SplashScreen : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding= ActivitySplashScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        val sharedPreference=getSharedPreferences("forcounts", Context.MODE_PRIVATE)
+        val gender=sharedPreference.getString(Constants.USER_GENDER,"null")
+        /*
+        Checking if user has entered their details or not
+        if not start login activty
+        else start main activity
+         */
         Handler().postDelayed(
             {
-                val intent= Intent(this, LoginActivity::class.java)
-                startActivity(intent)
+                if(gender=="null")
+                {
+                    val intent= Intent(this, LoginActivity::class.java)
+                    startActivity(intent)
+                }
+                else
+                {
+                    val intent= Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                }
                 finish()
             },1600
         )
