e9d68640a566e7ec43d5cfa61d2cb13bcb75a35d
atif9 <mohammadatif92080@gmail.com>
2021-09-27 11:49:46 +0530

    history layout v1

--- app/src/main/java/com/example/trackpace/ui/fragments/HistoryFragment.kt
+++ app/src/main/java/com/example/trackpace/ui/fragments/HistoryFragment.kt
@@ -1,5 +1,6 @@
 package com.example.trackpace.ui.fragments
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -9,6 +10,7 @@ import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.trackpace.adapters.HistoryRecyclerViewAdapter
 import com.example.trackpace.databinding.FragmentHistoryBinding
+import com.example.trackpace.util.Constants
 import com.example.trackpace.util.TopSpacingDecoration
 import com.example.trackpace.viewmodels.TrackerViewModel
 
@@ -30,6 +32,17 @@ class HistoryFragment : Fragment() {
             historyRecyclerViewAdapter.submitList(it.toMutableList())
         })
         return binding.root
+
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val sharedPreference=activity!!.getSharedPreferences("forcounts", Context.MODE_PRIVATE)
+
+
+        binding.txtUserNameHistory.text=sharedPreference.getString(Constants.USER_NAME,"null")
     }
 
 
@@ -40,7 +53,7 @@ class HistoryFragment : Fragment() {
         binding.historyrecView.apply {
             layoutManager= LinearLayoutManager(activity)
             historyRecyclerViewAdapter= HistoryRecyclerViewAdapter()
-            val topspace= TopSpacingDecoration(10)
+            val topspace= TopSpacingDecoration(16)
             addItemDecoration(topspace)
             adapter= historyRecyclerViewAdapter
         }
