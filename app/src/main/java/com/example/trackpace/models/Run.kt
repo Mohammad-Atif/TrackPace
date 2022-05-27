07c4fe3f6e314abb76350d45279546cebfd91b97
atif9 <mohammadatif92080@gmail.com>
2022-05-28 02:50:25 +0530

    majaor changes start

--- app/src/main/java/com/example/trackpace/models/Run.kt
+++ app/src/main/java/com/example/trackpace/models/Run.kt
@@ -1,15 +1,18 @@
 package com.example.trackpace.models
 
+import android.graphics.Bitmap
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity
+@Entity(tableName = "running_table")
 data class Run(
-    val imageUri: String?,
-    val date: String?,
-    val distance: String?,
-    val duration: String?,
-    val calories : String?,
+    var img : Bitmap? = null,
+    var timestamp : Long = 0L,
+    var avgSpeedInKMH : Float = 0f,
+    var distanceInMeters : Int = 0,
+    var timeInMillis : Long = 0L,
+    var caloriesBurned : Int = 0
+    ){
     @PrimaryKey(autoGenerate = true)
-    var id: Int = 0
-)
+    var id : Int? = null
+}
